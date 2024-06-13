56516379f235f6a0dc01fdaa9148270745b02641
Reinhold Brant <reinholdb_@MacBook-Air-von-Reinhold.local>
2024-06-13 12:25:29 +0200

    player initial

--- Player/Player.hpp
+++ Player/Player.hpp
@@ -4,6 +4,10 @@
 class Player {
     private:
         int m_symbol = 1;
+    public:
+        Player();
+        virtual void move();
+    
 };
 
 #endif
---
+++ Player/main.cpp
@@ -0,0 +1,7 @@
+#include "Player.cpp"
+
+int main() {
+    Player p1 = Player();
+    p1.move();
+    return 1;
+}
